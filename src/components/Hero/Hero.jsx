76d3095d0400abac67919441f2b847e8afeec0c3
Anzal-F <anzalmohamed09@gmail.com>
2025-02-17 20:20:42 -0500

    Add Hero Section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Hero.scss";
+
+
+function Hero() {
+  return (
+    <div>
+        <p className="hero__title">
+            Our Mission: 
+        </p>
+        <p className="hero__text">
+            Provide photographers a 
+            space to share photos of the 
+            neighborhoods they 
+            cherish, <span>expressed in their 
+            unique style.</span>
+        </p>
+    </div>
+  )
+}
+
+export default Hero
