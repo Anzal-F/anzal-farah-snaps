d0ecaa1e7e0298f1c16675b0eec16636081a9a87
Anzal-F <anzalmohamed09@gmail.com>
2025-02-23 21:50:41 -0500

    create the form and add the former comments on the page

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -23,7 +23,47 @@ function Comments({ comments, newComment, handleCommentChange, handleSubmitComme
 
   return (
     <div className="comments-section">
-      
+      <form onSubmit={handleFormSubmit} className='form'>
+        <label className='name-label'>
+          Name 
+          <input 
+            className={`name-input ${errors.name ? 'input--invalid' : ''}`} 
+            type="text" 
+            name="name" 
+            value={newComment.name} 
+            onChange={handleCommentChange}
+          />
+          {errors.name && <p className="error-message">Name is required</p>}
+        </label>
+
+        <label className='name-label'>
+          Comment 
+          <input
+            className={`comment-input ${errors.comment ? 'input--invalid' : ''}`}
+            name='comment'
+            value={newComment.comment}
+            onChange={handleCommentChange}
+          />
+          {errors.comment && <p className="error-message">Comment is required</p>}
+        </label>
+
+        <button className='submit-btn' type="submit">Submit</button>
+      </form>
+
+      <h4 className='comments__title'>{comments.length} Comments</h4>
+      {comments.length > 0 ? (
+        comments.map((comment, index) => (
+          <div key={index} className="comment">
+            <div className="comment-upper">
+              <p>{comment.name}</p>
+              <p>{comment.timestamp ? new Date(comment.timestamp).toLocaleDateString() : "No date data"}</p>
+            </div>
+            <p className='former-comment'>{comment.comment}</p>
+          </div>
+        ))
+      ) : (
+        <p>No comments yet.</p>
+      )}
     </div>
   );
 }
