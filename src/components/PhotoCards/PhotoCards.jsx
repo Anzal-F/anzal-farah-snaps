91e9cd2542267883740eae33ed03ebd21c8e614d
Anzal-F <anzalmohamed09@gmail.com>
2025-02-23 21:28:17 -0500

    install axios and fetch the photos and comments from the URL

--- src/components/PhotoCards/PhotoCards.jsx
+++ src/components/PhotoCards/PhotoCards.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "./PhotoCards.scss";
 
 export default function PhotoCards({ photo }) {
   return (
-    <div className="photo-card">
-      <img src={photo.photo} alt={photo.photoDescription} className="photo-img" />
-      <h3 className='photo-photographer'>{photo.photographer}</h3>
-      <div className="tags-photo">
-            {photo.tags.map((tag, index) => (
-                <span key={index} className="tag">
-                {tag}
-                </span>
-            ))}
+    <Link to={`/photo/${photo.id}`} className="photo-card-link">
+      <div className="photo-card">
+        <img src={photo.photo} alt={photo.photoDescription} className="photo-img" />
+        <h3 className="photo-photographer">{photo.photographer}</h3>
+        <div className="tags-photo">
+          {photo.tags.map((tag, index) => (
+            <span key={index} className="tag">{tag}</span>
+          ))}
         </div>
-
-    </div>
+      </div>
+    </Link>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,11 +6,44 @@ const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
 const API_KEY = "f8599705-80b5-4d6f-8247-182da6f4e3ac";
 
 function HomePage() {
+  const [tags, setTags] = useState([]);
+  const [selectedTag, setSelectedTag] = useState(null);
+  const [photos, setPhotos] = useState([]);
+  const [filteredPhotos, setFilteredPhotos] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);  
 
+ useEffect(() => {
+    
+    const fetchData = async () => {
+        try {
+            const tagsResponse = await axios.get(`${API_URL}/tags?api_key=${API_KEY}`);
+            const photosResponse = await axios.get(`${API_URL}/photos?api_key=${API_KEY}`);
+    
+            setTags(tagsResponse.data);
+            setPhotos(photosResponse.data);
+            setFilteredPhotos(photosResponse.data)
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    fetchData();
+ }, []);
+
+  const handleFilterSelect = (tag) => {
+    if (selectedTag === tag) {
+      setSelectedTag(null);
+      setFilteredPhotos(photos);
+    } else {
+      setSelectedTag(tag);
+      setFilteredPhotos(photos.filter((photo) => photo.tags.includes(tag)));
+    }
+  };
+
+  const toggleNavbar = () => setIsOpen(!isOpen);  
 
   return (
     <>
-
+    
     </>
   );
 }
