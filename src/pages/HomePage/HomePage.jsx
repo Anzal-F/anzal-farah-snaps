337185ee69a3cb162d282b8166000064d0138fc1
Anzal-F <anzalmohamed09@gmail.com>
2025-02-23 21:24:30 -0500

    Create homePage component

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
+const API_KEY = "f8599705-80b5-4d6f-8247-182da6f4e3ac";
+
+function HomePage() {
+
+
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default HomePage;
